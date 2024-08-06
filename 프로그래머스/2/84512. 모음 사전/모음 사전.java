4d6f9497a817dcad3fa7ddd4d24b3c4408e321ad
Seyoung Hong <maiorem00@gmail.com>
2024-08-07 01:19:37 +0900

    [level 2] Title: 모음 사전, Time: 12.94 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 class Solution {
+    
+    Set<String> set = new TreeSet<>();
+    
+    String[] arr = {"A", "E", "I", "O", "U"};
+    
     public int solution(String word) {
         int answer = 0;
+
+        for(int i = 0; i < 5; ++i) {
+            dfs(arr[i],1);
+        }
+        int count = 0;
+        for( String str : set) {
+            count++;
+            if(str.equals(word)) {
+                return count;
+            }
+        }
         return answer;
+    }
+    public void dfs(String word, int depth) {
+        set.add(word);
+        if(depth == 5){
+            return;
+        }
+        for(int i=0; i<5;++i){
+            dfs(word+arr[i], depth+1);
+        }
     }
 }
